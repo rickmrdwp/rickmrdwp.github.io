9c526a170675b4a17d9bf3765c86b8fd8a544143
rickmrdwp <87022795+rickmrdwp@users.noreply.github.com>
2021-07-07 12:31:32 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyDmovq7T_-Ym7iutIjJIN1ABI33usaMmJo",
+    authDomain: "autenfi41.firebaseapp.com",
+    projectId: "autenfi41",
+    storageBucket: "autenfi41.appspot.com",
+    messagingSenderId: "777550911881",
+    appId: "1:777550911881:web:205487691c96b111382958",
+    measurementId: "G-YHTXKL0597"
+});
